ed0b01dbdcb205af41f7ddccf4eb77316ec3eb15
mattbreintebach <matheusbreitenbach01@gmail.com>
2023-03-02 12:17:19 -0300

    minor changes

--- get_fin_data.py
+++ get_fin_data.py
@@ -4,15 +4,9 @@ import datetime
 import warnings
 import numpy as np
 
-# exemplo
-ticker = "PETR4.SA" 
-start_date = "2020-01-01"
-end_date = "2022-01-01" 
 
-default= str(datetime.date.today())
-
-def get_close_price(ticker: str, start_date: str, end_date: str = default) -> pd.core.frame.DataFrame:
-
+default_end_date = str(datetime.date.today())
+def get_close_price(ticker: str, start_date: str, end_date: str = default_end_date) -> pd.core.frame.DataFrame:
     """
     function to get the adjusted close price from yfinance package.
     
@@ -21,18 +15,13 @@ def get_close_price(ticker: str, start_date: str, end_date: str = default) -> pd
         start_date: a string with the first date of the period.
         end_date: a string with the last date of the period; default option actual day.
     """
-    #start_date = datetime.date.today() - datetime.timedelta(years=timespan)
-    #end_date = datetime.date.today()
+
     df = yf.download(ticker, start=start_date, end=end_date)
     df = df.loc[:,"Adj Close"].to_frame()
+    df.rename(columns={"Adj Close": "adj_close"}, inplace=True)
+
 
     return df
-
-
-
-test = get_close_price(ticker,start_date, end_date)
-print(test)
-
 
 
 def normalize_price(stock: pd.core.frame.DataFrame, pct: float) -> pd.core.frame.DataFrame:
@@ -49,18 +38,20 @@ def normalize_price(stock: pd.core.frame.DataFrame, pct: float) -> pd.core.frame
     stock_outsample = stock.iloc[n:,:]
 
     price_max = np.max(stock_insample)
-    price_min = np.min(stock_outsample)
+    price_min = np.min(stock_insample)
     price_range = price_max - price_min
-    
-    norm_price_insample = stock_insample.sub(price_min).div(price_range)
-    norm_price_outsample = stock_outsample.sub(price_min).div(price_range)
 
-    norm_price = pd.concat([norm_price_insample, norm_price_outsample])
+    norm_price_insample = stock_insample.sub(price_min).div(price_range)
+    norm_price_insample.rename(columns={"adj_close": "adj_close_in"}, inplace=True)
+
+    norm_price_outsample = stock_outsample.sub(price_min).div(price_range)
+    norm_price_insample.rename(columns={"adj_close": "adj_close_out"}, inplace=True)
+    
+    norm_price = pd.concat([norm_price_outsample, norm_price_insample], axis=1)
 
     return norm_price
 
-test = normalize_price(test, 0.7)
-print(test.tail())
+
 
 '''
     if len(stock_one) != len(stock_two):
