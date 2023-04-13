a7415a9b409b21e48b7d0345a1bf2b120b8c8dfe
JoaoJungblut <joaoramosjungblut@hotmail.com>
2023-04-13 14:42:21 -0300

    created main pipe

--- controller.py
+++ controller.py
@@ -1,73 +1,127 @@
-import analysis_manager
-import get_fin_data as gfd
-import backtest
-from pairs_methods import distance_approach as dist
+from dash import dash, dcc, html, Input, Output, State
 import pandas as pd
 import plotly.express as px
 import datetime
 
+import get_fin_data as gfd
+from pairs_methods import distance_approach as dist
+import backtest
+import viewer
 
 
-"""
-def main_pipe(ticker_name, start_date, end_date):
-    pass
 
-
-default_end_date = str(datetime.date.today())
-
-def generate_zscore_fig(ticker_name1: str, ticker_name2: str, start_date: str, end_date: str = default_end_date, pct_insample: float = 0.7):
+def main_pipe(ticker_name1: str, ticker_name2: str, start_date: str):
+    """
+    
+    """
 
     # importing stocks
-    stock1 = gfd.get_close_price(ticker_name1, start_date, end_date)
-    stock2 = gfd.get_close_price(ticker_name2, start_date, end_date)
+    stock1 = gfd.get_close_price(ticker_name1, start_date)
+    stock2 = gfd.get_close_price(ticker_name2, start_date)
 
-    # spliting in-sample and ou-of-sample
+    # spliting train and test base
     stock1_train, stock1_test = backtest.split_train_test(stock1)
     stock2_train, stock2_test = backtest.split_train_test(stock2)
-
 
     # normalizing stocks
     stock1_train_norm, stock1_test_norm = dist.normalize_series(stock1_train, stock1_test)
     stock2_train_norm, stock2_test_norm = dist.normalize_series(stock2_train, stock2_test)
+    stock1_train_test_norm = pd.concat([stock1_train_norm, stock1_test_norm], axis=0)
+    stock2_train_test_norm = pd.concat([stock2_train_norm, stock2_test_norm], axis=0)
 
-    # calculting spread
+    # calculating spread
     spread_train = dist.spread_distance(stock1_train_norm, stock2_train_norm)
     spread_test = dist.spread_distance(stock1_test_norm, stock2_test_norm)
 
-    # calculating z-score 
+    # calculating z-score
     z_score_train, z_score_test = dist.Z_score(spread_train, spread_test)
     z_score_train_test = pd.concat([z_score_train, z_score_test], axis=0)
-    z_score_train_test.rename("Spread", inplace=True)
-    z_score_train_test = z_score_train_test.to_frame().reset_index()
 
-    # creating graphic
-    fig = px.line(data_frame=z_score_train_test, x="Date", y= "Spread", title=f"{ticker_name1} X {ticker_name2}: Spread")
+    # generating signal
+    signal = backtest.generate_signal(z_score_train_test, 1, 0)
 
-    return fig
+    # calculate stock returns
+    stock1_rtn = backtest.calculate_stock_return(stock1)
+    stock2_rtn = backtest.calculate_stock_return(stock2)
 
+    # calculate trade return
+    trade_return = backtest.calculate_trade_return(signal, stock1_rtn, stock2_rtn)
+    cum_trade_return = backtest.calculate_compound_return(trade_return)
 
-
-def generate_normalized_fig(ticker_name1: str, ticker_name2: str,start_date: str, end_date: str = default_end_date, pct_insample: float = 0.7):
-
-    stock1 = gfd.get_close_price(ticker_name1, "2021-01-01", "2023-01-01")
-    stock_train1, stock_test1 = backtest.split_train_test(stock1)
-    stock_train_norm1, stock_test_norm1 = dist.normalize_series(stock_train1, stock_test1)
-
-    stock2 = gfd.get_close_price(ticker_name2, "2021-01-01", "2023-01-01")
-    stock_train2, stock_test2 = backtest.split_train_test(stock2)
-    stock_train_norm2, stock_test_norm2 = dist.normalize_series(stock_train2, stock_test2)
+    # generating graphs
+    zscore_fig = viewer.generate_zscore_fig(z_score_train_test, signal, ticker_name1, ticker_name2)
+    normalized_price_fig = viewer.generate_normalized_fig(stock1_train_test_norm, stock2_train_test_norm, ticker_name1, ticker_name2)
+    trade_return_fig = viewer.generate_trade_returns_fig(cum_trade_return)
     
-    normalized1  = pd.concat([stock_train_norm1, stock_test_norm1], axis=0)
-    normalized1 = normalized1.to_frame().reset_index()
-    normalized1 = normalized1.rename(columns={'Adj Close': f'{ticker_name1}'})
+    return zscore_fig, normalized_price_fig, trade_return_fig
 
-    normalized2  = pd.concat([stock_train_norm2, stock_test_norm2], axis=0)
-    normalized2 = normalized2.to_frame().reset_index()
-    normalized2 = normalized2.rename(columns={'Adj Close': f'{ticker_name2}'})
 
-    normalized = pd.merge(normalized1, normalized2, how='left', on="Date")
-    normalized = normalized.set_index("Date")
-    fig = px.line(normalized, title=f"{ticker_name1} X {ticker_name2}: Normalized price")
-    return fig
-""" 
 
+# creating first fig
+zscore_fig, normalized_price_fig, trade_return_fig = main_pipe("PETR4.SA", "ITSA4.SA", "2021-01-01")
+
+
+
+# creating app
+app = dash.Dash(__name__)
+
+app.layout = html.Div(children=[
+    html.Div([
+        html.H1("Stocks"),
+        html.Label("Select ticker one", className='dropdown-labels'),
+        dcc.Dropdown(multi=False,
+                     id='ticker-1',
+                     options=[{'label': "Petrobras", 'value': "PETR4.SA"},
+                              {'label': "Itausa", 'value': "ITSA4.SA"},
+                              {'label': "Itau", 'value': "ITUB4.SA"},
+                              {'label': "Vale", 'value': "VALE3.SA"},
+                              {'label': "Ambev", 'value': "ABEV3.SA"},
+                              {'label': "Bradesco", 'value': "BBDC4.SA"}],
+                              value='PETR4.SA'),
+        html.Label("Select ticker two", className='dropdown-labels'), 
+        dcc.Dropdown(multi=False,
+                     id='ticker-2',
+                     options=[{'label': "Petrobras", 'value': "PETR4.SA"},
+                              {'label': "Itausa", 'value': "ITSA4.SA"},
+                              {'label': "Itau", 'value': "ITUB4.SA"},
+                              {'label': "Vale", 'value': "VALE3.SA"},
+                              {'label': "Ambev", 'value': "ABEV3.SA"},
+                              {'label': "Bradesco", 'value': "BBDC4.SA"}],
+                              value="ITSA4.SA"),
+        html.Button("Update", id="update_button"),
+        ]),
+    html.Div([
+        html.Div([
+            dcc.Graph(figure=zscore_fig, id='zscore_graph'),
+        ])
+    ]),
+    html.Div([
+        html.Div([
+            dcc.Graph(figure=normalized_price_fig, id='normalized_prices'),
+        ])
+    ]),
+    html.Div([
+        html.Div([
+            dcc.Graph(figure=trade_return_fig, id='zscore_graph'),
+        ])
+    ]),
+])
+
+
+
+# Connecting the Dropdown values to the graph
+@app.callback(
+    [Output(component_id='zscore_graph', component_property='figure'), 
+     Output(component_id='normalized_prices', component_property='figure')],
+    [Input(component_id='update_button', component_property='n_clicks')],
+    [State(component_id='ticker-1',component_property='value'),
+     State(component_id='ticker-2',component_property='value')],
+    prevent_initial_call=True
+    )
+def generate_graph(n, stock1, stock2):
+    zscore_fig, normalized_price_fig, trade_return_fig = main_pipe(stock1, stock2, "2021-01-01")
+    return [zscore_fig, normalized_price_fig, trade_return_fig]
+
+
+if __name__ == '__main__':
+    app.run_server()#debug=True)
--- viewer.py
+++ viewer.py
@@ -1,129 +1,57 @@
-from dash import dash, dcc, html, Input, Output, State
 import pandas as pd
 import plotly.express as px
-import datetime
-
-import get_fin_data as gfd
-from pairs_methods import distance_approach as dist
-import backtest
 
 
-default_end_date = str(datetime.date.today())
-
-def generate_zscore_fig(ticker_name1: str, ticker_name2: str, start_date: str, end_date: str = default_end_date, pct_insample: float = 0.7):
-
-    # importing stocks
-    stock1 = gfd.get_close_price(ticker_name1, start_date, end_date)
-    stock2 = gfd.get_close_price(ticker_name2, start_date, end_date)
-
-    # spliting in-sample and ou-of-sample
-    stock1_train, stock1_test = backtest.split_train_test(stock1)
-    stock2_train, stock2_test = backtest.split_train_test(stock2)
-
-
-    # normalizing stocks
-    stock1_train_norm, stock1_test_norm = dist.normalize_series(stock1_train, stock1_test)
-    stock2_train_norm, stock2_test_norm = dist.normalize_series(stock2_train, stock2_test)
-
-    # calculting spread
-    spread_train = dist.spread_distance(stock1_train_norm, stock2_train_norm)
-    spread_test = dist.spread_distance(stock1_test_norm, stock2_test_norm)
-
-    # calculating z-score 
-    z_score_train, z_score_test = dist.Z_score(spread_train, spread_test)
-    z_score_train_test = pd.concat([z_score_train, z_score_test], axis=0)
-    z_score_train_test.rename("Spread", inplace=True)
-    z_score_train_test = z_score_train_test.to_frame().reset_index()
-
-    # creating graphic
-    fig = px.line(data_frame=z_score_train_test, x="Date", y= "Spread", title=f"{ticker_name1} X {ticker_name2}: Spread")
-
-    return fig
-
-
-
-def generate_normalized_fig(ticker_name1: str, ticker_name2: str,start_date: str, end_date: str = default_end_date, pct_insample: float = 0.7):
-
-    stock1 = gfd.get_close_price(ticker_name1, "2021-01-01", "2023-01-01")
-    stock_train1, stock_test1 = backtest.split_train_test(stock1)
-    stock_train_norm1, stock_test_norm1 = dist.normalize_series(stock_train1, stock_test1)
-
-    stock2 = gfd.get_close_price(ticker_name2, "2021-01-01", "2023-01-01")
-    stock_train2, stock_test2 = backtest.split_train_test(stock2)
-    stock_train_norm2, stock_test_norm2 = dist.normalize_series(stock_train2, stock_test2)
+def generate_zscore_fig(z_score: pd.core.series.Series, signal: pd.core.series.Series, ticker_name1: str, ticker_name2: str):
+    """
     
-    normalized1  = pd.concat([stock_train_norm1, stock_test_norm1], axis=0)
-    normalized1 = normalized1.to_frame().reset_index()
-    normalized1 = normalized1.rename(columns={'Adj Close': f'{ticker_name1}'})
+    """
 
-    normalized2  = pd.concat([stock_train_norm2, stock_test_norm2], axis=0)
-    normalized2 = normalized2.to_frame().reset_index()
-    normalized2 = normalized2.rename(columns={'Adj Close': f'{ticker_name2}'})
+    z_score = z_score.rename("Spread", inplace=True)
+    z_score = z_score.to_frame().reset_index()
+    signal = signal.rename("Signal", inplace=True)
+    signal = signal.to_frame().reset_index()
 
-    normalized = pd.merge(normalized1, normalized2, how='left', on="Date")
-    normalized = normalized.set_index("Date")
-    fig = px.line(normalized, title=f"{ticker_name1} X {ticker_name2}: Normalized price")
+    df_zscore_signal = pd.merge(z_score, signal, how='left', on="Date")
+    df_zscore_signal = df_zscore_signal.set_index("Date")
+
+    fig = px.line(df_zscore_signal, title=f"{ticker_name1} X {ticker_name2}: Spread")
+
     return fig
 
 
-zscore_fig = generate_zscore_fig("PETR4.SA", "ITSA4.SA", "2021-01-01")
-normalized_price_fig = generate_normalized_fig("PETR4.SA", "ITSA4.SA", "2021-01-01")
+def generate_normalized_fig(stock1_norm: pd.core.series.Series, stock2_norm: pd.core.series.Series, ticker_name1: str, ticker_name2: str):
+    """
+    
+    """
 
-app = dash.Dash(__name__)
+    stock1_norm = stock1_norm.to_frame().reset_index()
+    stock1_norm = stock1_norm.rename(columns={'Adj Close': f'{ticker_name1}'})
 
-app.layout = html.Div(children=[
-    html.Div([
-        html.H1("Stocks"),
-        html.Label("Select ticker one", className='dropdown-labels'),
-        dcc.Dropdown(multi=False,
-                     id='ticker-1',
-                     options=[{'label': "Petrobras", 'value': "PETR4.SA"},
-                              {'label': "Itausa", 'value': "ITSA4.SA"},
-                              {'label': "Itau", 'value': "ITUB4.SA"},
-                              {'label': "Vale", 'value': "VALE3.SA"},
-                              {'label': "Ambev", 'value': "ABEV3.SA"},
-                              {'label': "Bradesco", 'value': "BBDC4.SA"}],
-                              value='PETR4.SA'),
-        html.Label("Select ticker two", className='dropdown-labels'), 
-        dcc.Dropdown(multi=False,
-                     id='ticker-2',
-                     options=[{'label': "Petrobras", 'value': "PETR4.SA"},
-                              {'label': "Itausa", 'value': "ITSA4.SA"},
-                              {'label': "Itau", 'value': "ITUB4.SA"},
-                              {'label': "Vale", 'value': "VALE3.SA"},
-                              {'label': "Ambev", 'value': "ABEV3.SA"},
-                              {'label': "Bradesco", 'value': "BBDC4.SA"}],
-                              value="ITSA4.SA"),
-        html.Button("Update", id="update_button"),
-        ]),
-    html.Div([
-        html.Div([
-            dcc.Graph(figure=zscore_fig, id='zscore_graph'),
-        ])
-    ]),
-    html.Div([
-        html.Div([
-            dcc.Graph(figure=normalized_price_fig, id='normalized_prices'),
-        ])
-    ]),
-])
+    stock2_norm = stock2_norm.to_frame().reset_index()
+    stock2_norm = stock2_norm.rename(columns={'Adj Close': f'{ticker_name1}'})
+
+    df_norm = pd.merge(stock1_norm, stock2_norm, how='left', on="Date")
+    df_norm = df_norm.set_index("Date")
+
+    fig = px.line(df_norm, title=f"{ticker_name1} X {ticker_name2}: Normalized price")
+
+    return fig
 
 
 
-# Connecting the Dropdown values to the graph
-@app.callback(
-    [Output(component_id='zscore_graph', component_property='figure'), 
-     Output(component_id='normalized_prices', component_property='figure')],
-    [Input(component_id='update_button', component_property='n_clicks')],
-    [State(component_id='ticker-1',component_property='value'),
-     State(component_id='ticker-2',component_property='value')],
-    prevent_initial_call=True
-    )
-def generate_graph(n, stock1, stock2):
-    zscore_fig = generate_zscore_fig(stock1, stock2, "2021-01-01")
-    normalized_fig = generate_normalized_fig(stock1, stock2, "2021-01-01")
-    return [zscore_fig, normalized_fig]
+
+def generate_trade_returns_fig(cum_trade_return: pd.core.series.Series):
+    """
+    
+    """
+
+    cum_trade_return = cum_trade_return.rename("Cumulative Return", inplace=True)
+    cum_trade_return = cum_trade_return.to_frame().reset_index()
+    cum_trade_return = cum_trade_return.set_index("Date")
+
+    fig = px.line(cum_trade_return, title=f"Profit")
+
+    return fig
 
 
-if __name__ == '__main__':
-    app.run_server()#debug=True)
